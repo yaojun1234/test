2ea9f6154d9224993134a61fe99b9e51f1656e79
yao_glb <666_glb@yao.com>
2018-10-24 09:28:03 +0800

    idea使用git提交哦啊

--- src/main/java/com/yao/bean/Person.java
+++ src/main/java/com/yao/bean/Person.java
@@ -2,6 +2,6 @@ package com.yao.bean;
 
 public class Person {
     public static void main(String[] args) {
-        System.out.println("这是我的项目，需要用git将版本控制起来11");
+        System.out.println("这是我的项目，需要用git将版本控制起来111");
     }
 }
